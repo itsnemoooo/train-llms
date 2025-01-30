c72b53d3ef6c58df29d4f5df6ba10adabcce2eba
mertnba <102730570+mertnba@users.noreply.github.com>
2025-01-30 11:55:27 +0000

    Add files via upload

---
+++ test/data.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+import math
+from hellaswag import render_example, iterate_examples
+import tiktoken
+import numpy as np
+import os
+from pathlib import Path
+
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = npt.astype(np.int32)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+
+class DataLoaderLite:
+
+    def __init__(self, B, T, process_rank, num_processes, split):
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.num_processes = num_processes
+
+        assert split in {'train', 'val'}
+
+        current_script_path = Path(__file__).resolve()
+        project_root = current_script_path.parents[2]
+        data_root = project_root / "edu_fineweb10B"
+
+        # load the shards (files)
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+
+        assert len(shards) > 0, f"No shards found for split {split}."
+
+        if self.process_rank == 0:
+            print(f"Found {len(shards)} shards for split {split}.")
+
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position: self.current_position + B * T + 1]
+
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B, T)
+
+        self.current_position += B * T * self.num_processes
+
+        if self.current_position + (B * T * self.num_processes + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = B * T * self.process_rank
+
+        return x, y
